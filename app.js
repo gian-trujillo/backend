5fad4cf34df24a3a21906ce6460eac26f000d832
gian-trujillo <giantrujillo98@gmail.com>
2026-05-27 15:27:22 -0600

    fix cors preflight

--- app.js
+++ app.js
@@ -21,14 +21,19 @@ const app = express();
 const allowedCors = [
   'https://skylens.adventphoto.com',
   'https://www.skylens.adventphoto.com',
-  'https://api.skylens.adventphoto.com',
 ];
 
-app.use(cors({
+const corsOptions = {
   origin: allowedCors,
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 app.use(requestLogger);
 
 app.use('/auth', authRoutes);
@@ -51,7 +56,7 @@ app.use((err, req, res, next) => {
 
 const { PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Access the server at http://localhost:${PORT}`);
 });
