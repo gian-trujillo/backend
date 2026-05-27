e6cca9b650df8c65a0686b24880139e4ec7b423b
gian-trujillo <giantrujillo98@gmail.com>
2026-05-27 16:33:43 -0600

    fix cors outdated issue

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
