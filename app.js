ed3e9d7efa5972d8b598462ddda36ac0db5b549d
gian-trujillo <giantrujillo98@gmail.com>
2026-05-27 15:44:47 -0600

    fix size upload issue

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(requestLogger);
 
 app.use('/auth', authRoutes);
